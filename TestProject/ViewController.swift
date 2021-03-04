226cf0c451c6b11ecffd9e4abfd9941344b36959
ZelimhanEdilov <savachoc@mail.ru>
2021-03-04 06:30:25 +0300

    Initial Commit

---
+++ TestProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestProject
+//
+//  Created by MacBook on 04.03.2021.
+//  Copyright © 2021 ZelimkhanEdilov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
